326346d730900c7f7affac8f59fb8a6e448d04cc
eleveyuan <eleveyuan@gmail.com>
2024-04-17 13:41:56 +0800

    init proj

---
+++ gen.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+set -o errexit
+
+LOCATION=antlr-4.9.3-complete.jar
+# -Xexact-output-dir for issue(https://github.com/antlr/antlr4/issues/638 and https://github.com/antlr/antlr4/pull/2065)
+java -jar $LOCATION -Dlanguage=Cpp -listener -visitor -Xexact-output-dir -o generated/CMake/ resource/CMake/CMake.g4
